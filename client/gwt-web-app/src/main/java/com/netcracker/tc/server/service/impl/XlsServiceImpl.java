8c5e9090628ab2941594f51576295e7f77c36139
Anna Tsurkan <annatsurkan.23@gmail.com>
2020-01-22 22:25:57 +0200

    Made DetailInfo and XlsService pretty?

--- client/gwt-web-app/src/main/java/com/netcracker/tc/server/service/impl/XlsServiceImpl.java
+++ client/gwt-web-app/src/main/java/com/netcracker/tc/server/service/impl/XlsServiceImpl.java
@@ -17,13 +17,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
